e7bc770c93bd8e7656d8e9f033b09369b64a21c6
Smirn0v-da <dan16533_2@mail.ru>
2020-10-22 18:15:24 +0300

    Увеличение FPS и числа шариков

--- balls.py
+++ balls.py
@@ -3,7 +3,7 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 10
+FPS = 30
 screen = pygame.display.set_mode((800, 600))
 
 RED = (255, 0, 0)
@@ -15,51 +15,63 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-def new_ball():
+k = 10
+n = 0
+x = [0] * k
+y = [0] * k
+Vx = [0] * k
+Vy = [0] * k
+r = [0] * k
+color = [0] * k
+
+def new_ball(i):
     global x, y, r, Vx, Vy, color
-    x = randint(100,700)
-    y = randint(100,500)
-    r = randint(30,50)
-    Vx = randint(-10, 10)
-    Vy = randint(-10, 10)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    x[i] = randint(100,700)
+    y[i] = randint(100,500)
+    r[i] = randint(30,50)
+    Vx[i] = randint(-10, 10)
+    Vy[i] = randint(-10, 10) 
+    color[i] = COLORS[randint(0, 5)]
+    circle(screen, color[i], (x[i], y[i]), r[i])
 
-def ball_move():
-    global x, y, Vx, Vy, r
-    x += Vx
-    y += Vy
-    if x - r <= 50:
-        Vx = -Vx
-        x =  50 + r
-    if x + r >= 750:
-        Vx = -Vx
-        x = 750 - r
-    if y - r <= 50:
-        Vy = -Vy
-        y =  50 + r
-    if y + r >= 550:
-        Vy = -Vy
-        y = 550 - r   
-    screen.fill(BLACK)
-    circle(screen, color, (x, y), r)
+def ball_move(i):
+    global x, y, r, Vx, Vy, color
+    circle(screen, BLACK, (x[i], y[i]), r[i])
+    x[i] += Vx[i]
+    y[i] += Vy[i]
+    if x[i] - r[i] <= 50:
+        Vx[i] = -Vx[i]
+        x[i] =  50 + r[i]
+    if x[i] + r[i] >= 750:
+        Vx[i] = -Vx[i]
+        x[i] = 750 - r[i]
+    if y[i] - r[i] <= 50:
+        Vy[i] = -Vy[i]
+        y[i] =  50 + r[i]
+    if y[i] + r[i] >= 550:
+        Vy[i] = -Vy[i]
+        y[i] = 550 - r[i]   
+    circle(screen, color[i], (x[i], y[i]), r[i])
 
-def click(event):
+def click(event, k):
     global n
-    x1 = event.pos[0]
-    y1 = event.pos[1]
-    if (x1 - x) ** 2 + (y1 - y) **2 <= r**2:
-        n += 1
-    screen.fill(BLACK)
-    new_ball()
+    event.x = event.pos[0]
+    event.y = event.pos[1]
+    for i in range(0, k):
+        if (event.x - x[i]) ** 2 + (event.y - y[i]) **2 <= r[i]**2:
+            n += 1
+            circle(screen, BLACK, (x[i], y[i]), r[i])
+            new_ball(i)
     pygame.display.update()
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-n = 0
-new_ball()
+
+
+for i in range(0, k):
+    new_ball(i)
 
 while not finished:
     clock.tick(FPS)
@@ -68,8 +80,9 @@ while not finished:
             finished = True
             print('Ваши очки:',n)
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            click(event)
-    ball_move()
+            click(event, k)
+    for i in range(0, k):
+        ball_move(i)
     pygame.display.update()
 
 pygame.quit()
