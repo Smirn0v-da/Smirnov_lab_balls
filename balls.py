a5051a1ad151105d18a2db120c2eddad2cc14ec6
Smirn0v-da <dan16533_2@mail.ru>
2020-10-22 16:38:32 +0300

    Добавлена заготовка программы

---
+++ balls.py
@@ -0,0 +1,47 @@
+import pygame
+from pygame.draw import *
+from random import randint
+pygame.init()
+
+FPS = 0.5
+screen = pygame.display.set_mode((1200, 900))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+def new_ball():
+    global x, y, r
+    x = randint(100,700)
+    y = randint(100,500)
+    r = randint(30,50)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x, y), r)
+
+def click(event):
+    x1 = event.pos[0]
+    y1 = event.pos[1]
+    if (x1 - x) ** 2 + (y1 - y) **2 <= r**2:
+        print('+1')
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            click(event)
+    new_ball()
+    pygame.display.update()
+    screen.fill(BLACK)
+
+pygame.quit()
