235c8f4fe01ddcaec524c069034d8ddf0fc8f9ce
Smirn0v-da <dan16533_2@mail.ru>
2020-10-22 23:10:09 +0300

    Stonks!

--- balls.py
+++ balls.py
@@ -6,9 +6,10 @@ pygame.init()
 #длина и ширина экрана 
 a = 800
 b = 600
-
-FPS = 30
 screen = pygame.display.set_mode((a, b))
+
+#установка частоты кадров 
+FPS = 30
 
 #цвета 
 RED = (255, 0, 0)
@@ -23,7 +24,7 @@ COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 score = 0
 
 #количество мишеней разных видов
-balls_quantity = 0
+balls_quantity = 10
 squares_quantity = 5
 
 #списки с данными для круглых мишеней
@@ -44,7 +45,22 @@ squares_color = [0] * squares_quantity
 squares_x0 = [0] * squares_quantity
 squares_y0 = [0] * squares_quantity
 
+def draw_ball(i):
+    '''
+    Функция рисует круглую мишень c заданными параметрами
+    '''
+    circle(screen, balls_color[i], (balls_x[i], balls_y[i]), balls_r[i])
+    circle(screen, BLACK, (balls_x[i], balls_y[i]), balls_r[i], 1)
+
 def new_ball(i):
+    '''
+    Функция задает параметры новой круглой мишени и рисует её
+    i - номер мишени
+    balls_x, balls_y - координаты центра мишени
+    balls_r - радиус мишени
+    balls_Vx, balls_Vy - проекции скоростей мишени на оси
+    balls_color - цвет мишени
+    '''
     global balls_x, balls_y, balls_r, balls_Vx, balls_Vy, balls_color
     balls_x[i] = randint(100, a - 100)
     balls_y[i] = randint(100, b - 100)
@@ -52,24 +68,43 @@ def new_ball(i):
     balls_Vx[i] = randint(-10, 10)
     balls_Vy[i] = randint(-10, 10) 
     balls_color[i] = COLORS[randint(0, 5)]
-    circle(screen, balls_color[i], (balls_x[i], balls_y[i]), balls_r[i])
+    draw_ball(i)
 
+def draw_square(i):
+    '''
+    Функция рисует квадратную мишень с заданными параметрами
+    '''
+    rect(screen, squares_color[i], (squares_x[i] - squares_r[i], squares_y[i] - squares_r[i],
+                                    squares_r[i] * 2, squares_r[i] * 2))
+    rect(screen, BLACK, (squares_x[i] - squares_r[i], squares_y[i] - squares_r[i],
+                                    squares_r[i] * 2, squares_r[i] * 2), 1)
+    
 def new_square(i):
+    '''
+    Функция задает параметры новой квадратной мишени и рисует её
+    i - номер мишени
+    squares_x, squares_y - координаты центра мишени
+    squares_r - половина длины мишени
+    squares_x0, squares_y0 - координаты точки, к которой будет двигаться мишень
+    squares_Vx, squares_Vy - проекции скоростей мишени на оси
+    squares_color - цвет мишени
+    '''
     global squares_x, squares_y, squares_Vx, squares_Vy, squares_r, squares_color, squares_x0, squares_y0
     squares_x[i] = randint(150, a - 150)
     squares_y[i] = randint(150, b - 150)
     squares_r[i] = randint(10,20)
     squares_x0[i] = randint(squares_r[i], a - squares_r[i])
     squares_y0[i] = randint(squares_r[i], b - squares_r[i])
-    squares_Vx[i] = randint(3, 5)
-    squares_Vy[i] = randint(3, 5)
+    squares_Vx[i] = randint(5, 10)
+    squares_Vy[i] = randint(5, 10)
     squares_color[i] = COLORS[randint(0, 5)]
-    rect(screen, squares_color[i], (squares_x[i] - squares_r[i], squares_y[i] - squares_r[i],
-                                    squares_r[i] * 2, squares_r[i] * 2))
+    draw_square(i)
 
 def ball_move(i):
+    '''
+    Функция для движения круглой мишени
+    '''
     global balls_x, balls_y, balls_r, balls_Vx, balls_Vy, balls_color
-    circle(screen, BLACK, (balls_x[i], balls_y[i]), balls_r[i])
     balls_x[i] += balls_Vx[i]
     balls_y[i] += balls_Vy[i]
 
@@ -85,13 +120,16 @@ def ball_move(i):
         balls_y[i] =  50 + balls_r[i]
     if balls_y[i] + balls_r[i] >= b - 50:
         balls_Vy[i] = -balls_Vy[i]
-        balls_y[i] = b - 50 - balls_r[i]   
-    circle(screen, balls_color[i], (balls_x[i], balls_y[i]), balls_r[i])
+        balls_y[i] = b - 50 - balls_r[i]
+        
+    draw_ball(i)
 
 def square_move(i):
+    '''
+    Функция для движения квадратной мишени к точке (squares_x0, squares_y0)
+    Если одна из координат мишени достигает нужного значения, то её скорость по другой оси увеличивается
+    '''
     global squares_x, squares_y, squares_Vx, squares_Vy
-    rect(screen, BLACK, (squares_x[i] - squares_r[i], squares_y[i] - squares_r[i],
-                                    squares_r[i] * 2, squares_r[i] * 2))
     if squares_x[i] == squares_x0[i] and squares_y[i] == squares_y0[i]:
         new_square(i)
     else:
@@ -119,24 +157,27 @@ def square_move(i):
             else:
                squares_y[i] = squares_y0[i]
                squares_Vx[i] += squares_Vx[i]
-        rect(screen, squares_color[i], (squares_x[i] - squares_r[i], squares_y[i] - squares_r[i],
-                                    squares_r[i] * 2, squares_r[i] * 2))
+        draw_square(i)
         
 def click(event):
+    '''
+    Функция считывает, попали ли мы по мишени, и ведет счет.
+    При попадании мишень исчезает и рисуется новая
+    '''
     global score
+    #координаты курсора
     event.x = event.pos[0]
     event.y = event.pos[1]
     for i in range(0, balls_quantity):
         if (event.x - balls_x[i]) ** 2 + (event.y - balls_y[i]) **2 <= balls_r[i]**2:
-            score += 1
-            circle(screen, BLACK, (balls_x[i], balls_y[i]), balls_r[i])
+            score += 1 #количество очков за круглую мишень
+            screen.fill(BLACK)
             new_ball(i)
     for i in range (0, squares_quantity):
         if (squares_x[i] - squares_r[i] <= event.x) and (event.x <= squares_x[i] + squares_r[i]):
                 if (squares_y[i] - squares_r[i] <= event.y) and (event.y <= squares_y[i] + squares_r[i]):
-                    score += 2
-                    rect(screen, BLACK, (squares_x[i] - squares_r[i], squares_y[i] - squares_r[i],
-                                            squares_r[i] * 2, squares_r[i] * 2))
+                    score += 2 #количество очков за квадратную мишень
+                    screen.fill(BLACK)
                     new_square(i)
     pygame.display.update()
 
@@ -144,6 +185,7 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+#рисование первых мишеней 
 for i in range(0, balls_quantity):
     new_ball(i)
 for i in range(0, squares_quantity):
@@ -154,13 +196,17 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-            print('Ваш счет:',score)
+            print('Ваш счет:',score) #выводит счет игрока по окончании игры 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
+            
+    #двигаем мишени
     for i in range(0, balls_quantity):
         ball_move(i)
     for i in range(0, squares_quantity):
         square_move(i)
+        
     pygame.display.update()
+    screen.fill(BLACK)
 
 pygame.quit()
